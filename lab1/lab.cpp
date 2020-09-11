b344cf2136b3c4d518ef43efb51e8ca110744001
Vasiliy Molochkov <vasya.molochkof@gmail.com>
2020-09-11 15:36:56 +0300

    added FreeMemory

--- lab1/lab.cpp
+++ lab1/lab.cpp
@@ -8,7 +8,7 @@ using json = nlohmann::json;
 
 int ReadInit(const std::string path); //read configs
 
-int AllocateMemory(my_type** &matrixA, my_type* &columnB, const size_t n);
+int AllocateMemory(my_type** &matrixA, my_type** &matrixQ, my_type** matrixR, my_type* &columnB, const size_t n);
 
 int ReadData(const std::string path, my_type** &matrixA, my_type* columnB, const size_t n); //read matrix and column
 
@@ -16,7 +16,7 @@ int Calculations(my_type** &matrixA, my_type* &matrixB);
 
 int WriteData(my_type** matrixQ, my_type** matrixR, my_type* columnX); //write results in output file
 
-int FreeMemory(my_type** &matrixA, my_type* &matrixB); 
+int FreeMemory(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_type* &columnB, size_t n); 
 
 int MatrixMult(my_type** &matrixA,  my_type** &matrixB, const size_t n); //mult of 2 n*n matrix
 int MatrixMunt(my_type** &matrix, my_type* &column, const size_t n); //mult of n*n matrix on 1*n
@@ -24,13 +24,18 @@ int MatrixMunt(my_type** &matrix, my_type* &column, const size_t n); //mult of n
 int QRDecomposer(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, const size_t n);
 
 
-int AllocateMemory(my_type** &matrixA, my_type* &columnB, const size_t n)
+int AllocateMemory(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_type* &columnB, const size_t n)
 {
     matrixA = new my_type*[n];
-    
+    matrixQ = new my_type*[n];
+    matrixR = new my_type*[n];
+
+
     for(int i = 0; i < n; ++i)
     {
         matrixA[i] = new my_type[n];
+        matrixQ[i] = new my_type[n];
+        matrixR[i] = new my_type[n];
     }
     
     columnB = new my_type[n];
@@ -56,6 +61,23 @@ int ReadData(const std::string path, my_type** &matrixA, my_type* &columnB, cons
     return 0;
 }
 
+int FreeMemory(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_type* &columnB, size_t n)
+{
+    delete[] columnB;
+
+    for(int i = 0; i < n; ++i)
+    {
+        delete[] matrixA[i];
+        delete[] matrixQ[i];
+        delete[] matrixR[i];
+    }
+
+    delete[] matrixA;
+    delete[] matrixQ;
+    delete[] matrixR;
+
+    return 0;
+}
 
 int main()
 {
