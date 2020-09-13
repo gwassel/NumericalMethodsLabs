6f8881cd08564e8d6a201697482d4ff5095d79c9
Vasiliy <vasya.molochkof@gmail.com>
2020-09-13 13:52:02 +0300

    added input and output from files and data folder for io files

--- lab1/lab.cpp
+++ lab1/lab.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "../nlohmann/json.hpp"
 
@@ -6,30 +7,35 @@
 
 using json = nlohmann::json;
 
+//header
 int ReadInit(const std::string path); //read configs
 
-int AllocateMemory(my_type** &matrixA, my_type** &matrixQ, my_type** matrixR, my_type* &columnB, const size_t n);
+int AllocateMemory(my_type** &matrixA, my_type** &matrixQ, my_type** matrixR, my_type* &vectorB, my_type* &columnX, const size_t n);
 
-int ReadData(const std::string path, my_type** &matrixA, my_type* columnB, const size_t n); //read matrix and column
+int ReadData(const std::string fileNameMatrix, const std::string fileNameVector, my_type** &matrixA, my_type* &vectorB, const size_t n); //read matrix and column
 
-int Calculations(my_type** &matrixA, my_type* &matrixB);
+int Calculations(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_type* &vectorB, my_type* &columnX, const size_t n); 
 
-int WriteData(my_type** matrixQ, my_type** matrixR, my_type* columnX); //write results in output file
+int WriteData(std::string fileNameQ, std::string fileNameR, std::string fileNameX, 
+        my_type** &matrixQ, my_type** &matrixR, my_type* &vectorX, const size_t n); //write results in output file
 
-int FreeMemory(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_type* &columnB, size_t n); 
+
+int FreeMemory(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_type* &vectorB, my_type* &columnX, const size_t n); 
 
 int MatrixMult(my_type** &matrixA,  my_type** &matrixB, const size_t n); //mult of 2 n*n matrix
-int MatrixMunt(my_type** &matrix, my_type* &column, const size_t n); //mult of n*n matrix on 1*n
+int MatrixMunt(my_type** &matrix, my_type* &vector, const size_t n); //mult of n*n matrix on 1*n
 
 int QRDecomposer(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, const size_t n);
 
+int WriteMatrix(const std::string FileNameOutput, const std::string label, my_type** &matrix, const size_t n);
+int WriteVector(const std::string FileNameOutput, const std::string label, my_type* &vector, const size_t n);
 
-int AllocateMemory(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_type* &columnB, const size_t n)
+//init
+int AllocateMemory(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_type* &vectorB, const size_t n)
 {
     matrixA = new my_type*[n];
     matrixQ = new my_type*[n];
     matrixR = new my_type*[n];
-
 
     for(int i = 0; i < n; ++i)
     {
@@ -38,32 +44,62 @@ int AllocateMemory(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, m
         matrixR[i] = new my_type[n];
     }
     
-    columnB = new my_type[n];
+    vectorB = new my_type[n];
     
     return 0;
 }
 
-int ReadData(const std::string path, my_type** &matrixA, my_type* &columnB, const size_t n)
+int ReadData(const std::string fileNameMatrix, const std::string fileNameVector, my_type** &matrixA, my_type* &vectorB, const size_t n)
 {
-    for(int i = 0; i < n; ++i)
+    std::ifstream matrixFile;
+    matrixFile.open(fileNameMatrix);
+    
+    if( !matrixFile.is_open() )
     {
-        for(int j = 0; j < n; ++j)
-        {
-            std::cin >> matrixA[i][j];
-        }
+        std::cerr << "Error: file with matrix is not open\n";
+        return 1;
     }
 
     for(int i = 0; i < n; ++i)
     {
-        std::cin >> columnB[i];
+        for(int j = 0; j < n; ++j)
+        {
+            matrixFile >> matrixA[i][j];
+        }
+    }
+
+    matrixFile.close();
+
+    std::ifstream vectorFile;
+    vectorFile.open(fileNameVector);
+
+    if( !vectorFile.is_open() )
+    {
+        std::cerr << "Error: file with vector is not open\n";
+        return 1;
+    }
+
+    for(int i = 0; i < n; ++i)
+    {
+        vectorFile >> vectorB[i];
     }
 
     return 0;
 }
 
-int FreeMemory(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_type* &columnB, size_t n)
+int WriteData(std::string fileNameQ, std::string fileNameR, std::string fileNameX,
+        my_type** &matrixQ, my_type** &matrixR, my_type* &vectorX, const size_t n)
 {
-    delete[] columnB;
+    WriteMatrix(fileNameQ, "matrix Q", matrixQ, n);
+    WriteMatrix(fileNameR, "matrix R", matrixR, n);
+    WriteVector(fileNameX, "vector X", vectorX, n);
+
+    return 0;
+}
+
+int FreeMemory(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_type* &vectorB, const size_t n)
+{
+    delete[] vectorB;
 
     for(int i = 0; i < n; ++i)
     {
@@ -79,19 +115,70 @@ int FreeMemory(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_ty
     return 0;
 }
 
+int WriteMatrix(const std::string fileNameOutput, const std::string label, my_type** &matrix, const size_t n)
+{
+    std::ofstream fileOutput;
+    fileOutput.open(fileNameOutput);
+    
+    fileOutput << label << "\n";
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = 0; j < n; ++j){
+            fileOutput << matrix[i][j] << " ";
+        }
+        fileOutput << "\n";
+    }
+    fileOutput << "\n";
+    
+    fileOutput.close();
+
+    return 0;
+}
+
+int WriteVector(std::string fileNameOutput, const std::string label, my_type* &vector, const size_t n)
+{
+    std::ofstream fileOutput;
+    fileOutput.open(fileNameOutput);
+
+    fileOutput << label << "\n";
+    for(int i = 0; i < n; ++i)
+    {
+        fileOutput << vector[i] << "\n";
+    }
+    fileOutput << "\n";
+
+    fileOutput.close();
+
+    return 0;
+}
+
 int main()
 {
     my_type** matrixA;
-    my_type* columnB;
-    my_type* columnX;
+    my_type* vectorB;
+    my_type* vectorX;
     my_type** matrixQ;
     my_type** matrixR;
     
-    const char pathInit[] = "configs/config.json";
-    const char pathData[] = "data/";
+    const std::string pathConfig = "configs/config.json";
+    const std::string pathData = "data/";
     
-    size_t n = 0;
+    std::string fileNameMatrix = pathData + "matrix";
+    std::string fileNameVector = pathData + "vector";
+    
+    std::string fileNameQ = pathData + "outputQ";
+    std::string fileNameR = pathData + "outputR";
+    std::string fileNameX = pathData + "outputX";
 
+    size_t n = 5;
+
+    AllocateMemory(matrixA, matrixQ, matrixR, vectorB, n);
+
+    ReadData(fileNameMatrix, fileNameVector, matrixA, vectorB, n);   
+    
+    WriteData(fileNameQ, fileNameR, fileNameX, matrixA, matrixA, vectorB, n);
+
+    FreeMemory(matrixA, matrixQ, matrixR, vectorB, n);
 
     return 0;
 }
