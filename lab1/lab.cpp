127ce233a78380efcaa91428945dbc9532f70062
Vasiliy Molochkov <vasya.molochkof@gmail.com>
2020-09-15 09:27:51 +0300

    change AllocateMemory, add MatrixMult for matrix*vector

--- lab1/lab.cpp
+++ lab1/lab.cpp
@@ -12,6 +12,8 @@ using json = nlohmann::json;
 int ReadInit(const std::string path); //read configs
 
 int AllocateMemory(my_type** &matrixA, my_type** &matrixQ, my_type** matrixR, my_type* &vectorB, my_type* &columnX, const size_t n);
+int AllocateMemory(my_type** &matrix, const size_t n);
+int AllocateMemory(my_type* &vector, const size_t n);
 
 int ReadData(const std::string fileNameMatrix, const std::string fileNameVector, my_type** &matrixA, my_type* &vectorB, const size_t n); //read matrix and column
 
@@ -21,6 +23,8 @@ int WriteData(std::string fileNameQ, std::string fileNameR, std::string fileName
         my_type** &matrixQ, my_type** &matrixR, my_type* &vectorX, const size_t n); //write results in output file
 
 int FreeMemory(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_type* &vectorB, my_type* &columnX, const size_t n); 
+int FreeMemory(my_type** &matrix, const size_t n);
+int FreeMemory(my_type* &vector, const size_t n);
 
 int MatrixMult(my_type** &matrixA,  my_type** &matrixB, const size_t n); //mult of 2 n*n matrix
 int MatrixMult(my_type** &matrix, my_type* &vector, const size_t n); //mult of n*n matrix on 1*n
@@ -30,6 +34,7 @@ int MatrixTranspose(my_type** &matrixInit, my_type** &matrixResult, const size_t
 int QRDecomposer(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, const size_t n);
 int GetMatrixT(my_type** &matrixA, my_type** &matrixT, const size_t n);
 int GetMatrixI(my_type** &matrix, const size_t n);
+int SolveUpperTriangle(my_type** &matrixA, my_type* &vectorX, my_type* &vectorB); // solve Ax = b
 
 int WriteMatrix(const std::string FileNameOutput, const std::string label, my_type** &matrix, const size_t n); // write matrix to file
 int WriteMatrix(const std::string label, my_type** &matrix, const size_t n); // write matrix to console
@@ -38,19 +43,29 @@ int WriteVector(const std::string FileNameOutput, const std::string label, my_ty
 //init
 int AllocateMemory(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_type* &vectorB, const size_t n)
 {
-    matrixA = new my_type*[n];
-    matrixQ = new my_type*[n];
-    matrixR = new my_type*[n];
+    AllocateMemory(matrixA, n);
+    AllocateMemory(matrixQ, n);
+    AllocateMemory(matrixR, n);
+    AllocateMemory(vectorB, n);
+   
+    return 0;
+}
+
+int AllocateMemory(my_type** &matrix, const size_t n)
+{
+    matrix = new my_type*[n];
 
     for(int i = 0; i < n; ++i)
     {
-        matrixA[i] = new my_type[n];
-        matrixQ[i] = new my_type[n];
-        matrixR[i] = new my_type[n];
+        matrix[i] = new my_type[n];
     }
-    
-    vectorB = new my_type[n];
-    
+    return 0;
+}
+
+int AllocateMemory(my_type* &vector, const size_t n)
+{
+    vector = new my_type[n];
+
     return 0;
 }
 
@@ -94,11 +109,14 @@ int ReadData(const std::string fileNameMatrix, const std::string fileNameVector,
 
 int Calculations(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_type** &vectorB, my_type** &vectorX, const size_t n)
 {
+    
     QRDecomposer(matrixA, matrixQ, matrixR, n);
-    // QRDecomposion();
     
+    //b* = Tb    
+    //Rx = b*
+
+
     return 0;
-    
 }
 
 int WriteData(std::string fileNameQ, std::string fileNameR, std::string fileNameX,
@@ -113,18 +131,29 @@ int WriteData(std::string fileNameQ, std::string fileNameR, std::string fileName
 
 int FreeMemory(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, my_type* &vectorB, const size_t n)
 {
-    delete[] vectorB;
+    FreeMemory(matrixA, n);
+    FreeMemory(matrixQ, n);
+    FreeMemory(matrixR, n);
+    FreeMemory(vectorB, n);
 
+    return 0;
+}
+
+int FreeMemory(my_type** &matrix, const size_t n)
+{
     for(int i = 0; i < n; ++i)
     {
-        delete[] matrixA[i];
-        delete[] matrixQ[i];
-        delete[] matrixR[i];
+        delete[] matrix[i];
     }
 
-    delete[] matrixA;
-    delete[] matrixQ;
-    delete[] matrixR;
+    delete[] matrix;
+
+    return 0;
+}
+
+int FreeMemory(my_type* &vector, const size_t n)
+{
+    delete[] vector;
 
     return 0;
 }
@@ -147,7 +176,19 @@ int MatrixMult(my_type** &matrixA, my_type** &matrixB, my_type** &matrixResult, 
     return 0;
 }//matrixA * matrixB
 
-int MatrixMult(my_type** &matrix, my_type* &vector, my_type* &vectorResult){return (0/0);}//matrix * vector
+int MatrixMult(my_type** &matrix, my_type* &vector, my_type* &vectorResult, const size_t n)
+{
+    for(int i = 0; i < n; ++i)
+    {
+        my_type sum = 0;
+        for(int j = 0; j < n; ++j)
+        {
+            sum += matrix[i][j] * vector[j];
+        }
+        vectorResult[i] = sum;
+    }
+    return (0);
+}//matrix * vector
 
 int MatrixCopy(my_type** &matrixPaste, my_type** &matrixCopy, const size_t n)
 {
@@ -181,7 +222,6 @@ int QRDecomposer(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, con
     {
         matrixT[i] = new my_type[n];
     }
-    
 
     GetMatrixT(matrixA, matrixT, n);
 
@@ -190,8 +230,7 @@ int QRDecomposer(my_type** &matrixA, my_type** &matrixQ, my_type** &matrixR, con
     MatrixTranspose(matrixT, matrixQ, n); // Q = transpose(T)
 
     // b* = Tb
-    // Rx = b*
-    
+    // Rx = b*    
 
     for(int i = 0; i < n; ++i)
     {
@@ -275,6 +314,12 @@ int GetMatrixI(my_type** &matrix, const size_t n)
     }
 
     return 0;
+}
+
+int SolveUpperTriangle(my_type** matrixA, my_type* &vectorX, my_type* &vectorB)
+{
+    std::cout << "SolveUpperTrianble doesnt work";
+    return 0;     
 }
 
 int WriteMatrix(const std::string fileNameOutput, const std::string label, my_type** &matrix, const size_t n)
